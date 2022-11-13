789a525fbc9110d85bcfd3d1e4a79602044264bf
LJRollerII <ljcoolkid@optonline.net>
2022-11-12 20:40:09 -0500

    Defined summarize function. Added article retrieval and sentiment analysis to the function with updates. GUI section was also updated

--- scripts/news_summarizer.py
+++ scripts/news_summarizer.py
@@ -6,25 +6,46 @@ from newspaper import Article
 nltk.download('punkt')
 
 # Article Summary
-url = 'https://www.cnn.com/2019/09/25/tech/singapore-water-technology-innovative-cities/index.html'
 
-article = Article(url)
+def summarize():
+    url = utext.get('1.0', "end").strip()
 
-article.download()
-article.parse()
+    article = Article(url)
 
-article.nlp()
+    article.download()
+    article.parse()
 
-print(f'Title: {article.title}')
-print(f'Authors: {article.authors}')
-print(f'Date Published: {article.publish_date}')
-print(f'Summary: {article.summary}')
+    article.nlp()
 
-# Sentiment Analysis
-analysis = TextBlob(article.text)
-print(analysis.polarity)
-print(f'Sentiment: {"positve" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+    title.config(state='normal')
+    author.config(state='normal')
+    published.config(state='normal')
+    summary.config(state='normal')
+    sentiment.config(state='normal')
 
+    title.delete('1.0', 'end')
+    title.insert('1.0', article.title)
+
+    author.delete('1.0', 'end')
+    author.insert('1.0', article.authors)
+
+    published.delete('1.0', 'end')
+    published.insert('1.0', article.publish_date)
+
+    summary.delete('1.0', 'end')
+    summary.insert('1.0', article.summary)
+
+    # Sentiment Analysis
+    analysis = TextBlob(article.text)
+    sentiment.delete('1.0', 'end')
+    sentiment.insert('1.0', f'Polarity: {analysis.polarity}, Sentiment: {"positve" if analysis.polarity > 0 else "negative" if analysis.polarity < 0 else "neutral"}')
+
+    title.config(state='disabled')
+    author.config(state='disabled')
+    published.config(state='disabled')
+    summary.config(state='disabled')
+    sentiment.config(state='disabled')
+    
 # GUI
 root = tk.Tk()
 root.title("News Summarizer")
@@ -71,7 +92,7 @@ ulabel.pack()
 utext = tk.Text(root, height=1, width=140)
 utext.pack()
 
-btn = tk.Button(root, text="Summarize")
+btn = tk.Button(root, text="Summarize", command=summarize)
 btn.pack()
 
 root.mainloop()
